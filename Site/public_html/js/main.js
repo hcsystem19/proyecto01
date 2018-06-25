76992488fcb6680e56805bb781b3eed4561b1672
HCSYSTEM <HCSYSTEM@HCSYSTEM-VAIO.home>
2018-06-25 13:54:36 +0200

    Preparacion de slider2 cambio de imagenes y efectos al slider

--- Site/public_html/js/main.js
+++ Site/public_html/js/main.js
@@ -6,14 +6,20 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000
+        autoplaySpeed: 3000,
+        fade: true,
+        cssEase: 'linear'
     });
     $('.slider2').slick({
         dots: false,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        fade: true,
+        cssEase: 'linear'
     });
    
 });
