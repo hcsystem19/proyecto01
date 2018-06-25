d4a646274e6a0735f53a4bdb94380581df031c8c
hcsystem19 <etroncoso@hcsystem.es>
2018-06-25 12:09:03 +0200

    autoplay en slick slider

--- Site/public_html/js/main.js
+++ Site/public_html/js/main.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
         infinite: true,
         speed: 300,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true
     });
     $('.slider2').slick({
         dots: false,
